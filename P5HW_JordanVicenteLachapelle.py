651925c47892d2c06d9da229136710b26f3d0b2b
JordanVL1234 <96630742+JordanVL1234@users.noreply.github.com>
2023-11-16 14:35:33 -0500

    Doesn't work 😐

---
+++ P5HW_JordanVicenteLachapelle.py
@@ -0,0 +1,10 @@
+import random
+
+Num1 = round(random.random()*100)
+Num2 = round(random.random()*100)
+def AddRan(Num1, Num2):
+    print(Num1)
+    print(Num2)
+    print(Num1+Num2)
+    
+AddRan(Num1, Num2)
